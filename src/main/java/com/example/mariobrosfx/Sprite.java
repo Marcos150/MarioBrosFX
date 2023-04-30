5952f45225c8fdb2c02a5b98161d5ac883f78976
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-04-30 18:02:12 +0200

    Implemented class Sprite

--- src/main/java/com/example/mariobrosfx/Sprite.java
+++ src/main/java/com/example/mariobrosfx/Sprite.java
@@ -1,5 +1,7 @@
 package com.example.mariobrosfx;
 
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public abstract class Sprite
@@ -8,18 +10,45 @@ public abstract class Sprite
     private int width;
     private int x;
     private int y;
+    private int spriteX;
+    private int spriteY;
     private Image image;
 
-    void moveTo(int x, int y)
+    public Sprite(int width, int height)
     {
+        this.width = width;
+        this.height = height;
     }
 
-    void draw()
+    public void moveTo(int x, int y)
     {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(GraphicsContext gc)
+    {
+        gc.drawImage(image, spriteX, spriteY,
+                width, height, x, y, width, height);
+    }
+
+    public Rectangle2D getBoundary()
+    {
+        return new Rectangle2D(x, y, width, height);
     }
 
     boolean collidesWith(Sprite s)
     {
-        return true;
+        return s.getBoundary().intersects(this.getBoundary());
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
     }
 }
