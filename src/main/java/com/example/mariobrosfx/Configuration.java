65a15a0ba60b6ebe57d64aa854c0fbd3df0564c1
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-04-30 18:08:38 +0200

    Modified some configuration parameters

--- src/main/java/com/example/mariobrosfx/Configuration.java
+++ src/main/java/com/example/mariobrosfx/Configuration.java
@@ -3,15 +3,16 @@ package com.example.mariobrosfx;
 public class Configuration
 {
     //TODO find a proper screen size
-    public static final int SCREEN_WIDTH = 300;
-    public static final int SCREEN_HEIGHT = 150;
+    public static final int SCREEN_WIDTH = 816;
+    public static final int SCREEN_HEIGHT = 816;
     //TODO change this when I have the sprites sheets
     public static final int FRAME_ANIMATION_RATE = 10;
     public static final int INITIAL_LIVES = 3;
     public static final int POW_BLOCK_USAGES = 3;
     public static final int INITIAL_ENEMY_SPEED = 5;
-    public static final int GRAVITY = 8;
-    public static final int JUMP_FORCE = 2;
+    public static final int GRAVITY = 1;
+    public static final int MAX_GRAVITY = 15;
+    public static final int JUMP_FORCE = -15;
     //TODO fix secret code
     public static final String SECRET_CODE = "Up, Up, Down, Down, Left, Right, Left, Right, B, A";
 }
