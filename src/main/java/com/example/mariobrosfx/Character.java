788a0959d7c0f87da59be33dafa8c05c17abbf0e
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-04-30 18:18:35 +0200

    Implemented class Character

--- src/main/java/com/example/mariobrosfx/Character.java
+++ src/main/java/com/example/mariobrosfx/Character.java
@@ -1,17 +1,72 @@
 package com.example.mariobrosfx;
 
-public class Character extends AnimatedSprite
+import javafx.scene.image.Image;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Character extends Sprite
 {
     private int lives;
     private int points;
+    public static final int CHARACTER_WIDTH = 60;
+    public static final int CHARACTER_HEIGHT = 96;
+    private static final String IMAGE_PATH = "com/example/mariobrosfx/Sprites/sprites.png";
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    private boolean isOnPlatform;
+    private boolean canJump;
 
-    public void move()
+    public Character()
     {
+        super(CHARACTER_WIDTH, CHARACTER_HEIGHT);
+        try
+        {
+            image = new Image(Files.newInputStream(Paths.get(IMAGE_PATH)));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
+        lives = Configuration.INITIAL_LIVES;
+        isOnPlatform = false;
+        canJump = true;
+        spriteX = 18;
+        spriteY = 0;
+    }
+
+    public void move(int movement)
+    {
+        int newX = x;
+        if (movement == LEFT)
+            newX -= 2;
+        if (movement == RIGHT)
+            newX += 2;
+        moveTo(newX, y);
     }
 
     public void respawn()
     {
 
+    }
+
+    public boolean canJump()
+    {
+        return canJump;
+    }
+
+    public void setCanJump(boolean canJump)
+    {
+        this.canJump = canJump;
+    }
+
+    public boolean isOnPlatform()
+    {
+        return isOnPlatform;
+    }
+
+    public void setOnPlatform(boolean onPlatform)
+    {
+        isOnPlatform = onPlatform;
     }
 }
