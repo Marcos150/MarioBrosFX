ccf418d4aa33bd6c273ce3c69b2dede015b57f8e
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-04-30 17:55:40 +0200

    Just noticed that configuration variables weren't static and final, so I couldn't use them directly. This fixes that

--- src/main/java/com/example/mariobrosfx/Configuration.java
+++ src/main/java/com/example/mariobrosfx/Configuration.java
@@ -3,15 +3,15 @@ package com.example.mariobrosfx;
 public class Configuration
 {
     //TODO find a proper screen size
-    public int SCREEN_WIDTH = 300;
-    public int SCREEN_HEIGHT = 150;
+    public static final int SCREEN_WIDTH = 300;
+    public static final int SCREEN_HEIGHT = 150;
     //TODO change this when I have the sprites sheets
-    public int FRAME_ANIMATION_RATE = 10;
-    public int INITIAL_LIVES = 3;
-    public int POW_BLOCK_USAGES = 3;
-    public int INITIAL_ENEMY_SPEED = 5;
-    public int GRAVITY = 8;
-    public int JUMP_FORCE = 2;
+    public static final int FRAME_ANIMATION_RATE = 10;
+    public static final int INITIAL_LIVES = 3;
+    public static final int POW_BLOCK_USAGES = 3;
+    public static final int INITIAL_ENEMY_SPEED = 5;
+    public static final int GRAVITY = 8;
+    public static final int JUMP_FORCE = 2;
     //TODO fix secret code
-    public String SECRET_CODE = "Up, Up, Down, Down, Left, Right, Left, Right, B, A";
+    public static final String SECRET_CODE = "Up, Up, Down, Down, Left, Right, Left, Right, B, A";
 }
