eb13c2a6b0c8cb040b2775276f2a0034949cc0d9
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-05-15 16:56:44 +0200

    Added more sprites and implemented the AnimatedSprite class.

--- src/main/java/com/example/mariobrosfx/AnimatedSprite.java
+++ src/main/java/com/example/mariobrosfx/AnimatedSprite.java
@@ -4,6 +4,8 @@ public abstract class AnimatedSprite extends Sprite
 {
     public int HEIGHT;
     public int WIDTH;
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
     protected String SPRITE_FILE;
     protected int totalMovements;
     protected int currentMovement;
@@ -21,11 +23,29 @@ public abstract class AnimatedSprite extends Sprite
 
     public void animate(int movement)
     {
-
+        if (movement != currentMovement)
+        {
+            currentMovement = movement;
+            currentSprite = 0;
+            spriteChangeCounter = 0;
+        }
+        else
+        {
+            spriteChangeCounter++;
+            if (spriteChangeCounter >=
+                    Configuration.FRAME_ANIMATION_RATE)
+            {
+                spriteChangeCounter = 0;
+                currentSprite = (byte)((currentSprite + 1) %
+                        coordinatesX[currentMovement].length);
+            }
+        }
+        updateSpriteCoordinates();
     }
 
     protected void updateSpriteCoordinates()
     {
-
+        spriteX = coordinatesX[currentMovement][currentSprite];
+        spriteY = coordinatesY[currentMovement][currentSprite];
     }
 }
